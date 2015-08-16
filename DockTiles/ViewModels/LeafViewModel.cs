941548459dd06595308f7ff778b590a98b875a93
Matthew Wilkinson <Scfix@outlook.com>
2015-08-16 17:06:25 +0100

    The base foundation for the project

---
+++ DockTiles/ViewModels/LeafViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockTiles.ViewModels
+{
+    public class LeafViewModel : ViewModelBase
+    {
+        public object _Item;
+        public object Item
+        {
+            get
+            {
+                return _Item;
+            }
+            set
+            {
+                if (_Item != value)
+                {
+                    _Item = value;
+                    OnPropertyChanged("Item");
+                }
+
+            }
+        }
+
+    }
+}
---
+++ DockTiles/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockTiles.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
